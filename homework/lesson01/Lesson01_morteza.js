7e9685629adf7aa8c1205a9ad2a7d878c1b50d90
developerium <kheradmand.v1@gmail.com>
2019-08-21 11:50:03 +0430

    remove commented code

--- homework/lesson01/Lesson01_morteza.js
+++ homework/lesson01/Lesson01_morteza.js
@@ -11,23 +11,3 @@ const Stars=() => {
 }
 
 Stars();
-
-//----------------
-const Stars=() => {
-//     let st ='';
-//     for (let i = 1; i < 8 ; i++) {
-//         if (i<=4){
-//             for (let j=0; j<i ;j++){            
-//                 st=st+'*';
-//             }      
-//         }else{
-//             for (let j=0; j<(8-i) ;j++){ 
-//                 st=st+'*';                
-//             }  
-//         }
-//         console.log(st);
-//         st='';
-//     }
-// }
-
-// Stars();
