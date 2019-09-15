c4a507b161488a07addee4112269a9d7f7a2d252
raheleahar <54470437+raheleahar@users.noreply.github.com>
2019-09-15 09:04:16 +0430

    sang-kaghaz.js

---
+++ homework/Sang-Kaghaz.js
@@ -0,0 +1,18 @@
+x=1+Math.floor(Math.random()*3);
+y=1+Math.floor(Math.random()*3);
+console.log ('1 is kaghaz---- 2 is gheychi ---- 3 is sang');
+console.log('x: '+x);
+console.log('y: '+y);
+function SKG(x,y)
+{
+if(x>y)
+{
+console.log('The winner is x: '+x);
+}
+else
+{
+console.log('The winner is y: '+y);
+}
+}
+
+SKG(x,y);
